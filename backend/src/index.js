e29117609d892731d8bbd30c38f22085c1a12b6b
Prince-Singh-05 <luciefur6393473659@gmail.com>
2024-11-22 18:42:37 +0530

    cors setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,19 @@ import {} from "dotenv/config";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRouter from "./routes/message.route.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	})
+);
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
