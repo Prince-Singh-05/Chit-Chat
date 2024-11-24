7ee6806ce0b4b85e2399fcf06b96ebba6b0d3090
Prince-Singh-05 <luciefur6393473659@gmail.com>
2024-11-24 23:36:02 +0530

    socket server created in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,8 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRouter from "./routes/message.route.js";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json({ limit: "50mb" }));
@@ -22,7 +22,7 @@ app.use(
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log(`Server is up and running on port ${PORT}`);
 	connectDB();
 });
